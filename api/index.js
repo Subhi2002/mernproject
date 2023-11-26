ae7900409471937f8cea06dfa69d04444e88804e
Subhi2002 <75176547+Subhi2002@users.noreply.github.com>
2023-11-26 15:23:08 +0530

    connecting to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from  'express';
+import mongoose, { mongo } from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+const mongoURI = process.env.MONGO;
+mongoose.connect("mongodb+srv://subhi:subhi@cluster0.dyxhr52.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+    console.log('Connected to MongoDB');
+}).catch((err) =>{console.log(err);} );
+
 const app =express();
 app.listen(3000 , () =>{
  console.log('Server running on port 3000.....')
