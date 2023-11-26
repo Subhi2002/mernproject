b7c0029244a048a44833d64b9c4585d2f3e07f60
Subhi2002 <75176547+Subhi2002@users.noreply.github.com>
2023-11-26 16:32:10 +0530

    api and dtabase created

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from  'express';
 import mongoose, { mongo } from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 const mongoURI = process.env.MONGO;
 mongoose.connect("mongodb+srv://subhi:subhi@cluster0.dyxhr52.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
@@ -11,3 +12,5 @@ const app =express();
 app.listen(3000 , () =>{
  console.log('Server running on port 3000.....')
 });
+
+app.use('/api/user', userRouter);
